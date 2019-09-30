8ed6e9905718b83a29ca2000290dbc73268ec812
zac77543 <46709271+zac77543@users.noreply.github.com>
2019-09-30 18:14:30 +0800

    Update CloudFlare_DDNS_Setter.sh

--- CloudFlare_DDNS_Setter.sh
+++ CloudFlare_DDNS_Setter.sh
@@ -165,14 +165,14 @@ Lightsail_conf(){
 
 lightsail_change_ip(){
     #检查本机ip是否被tcp阻断
-    tcp_status=`curl --silent https://ipcheck.need.sh/api_v2.php?ip=${local_ip} | awk -F '[:}]' '{print $21}'`
-    if [[ $tcp_status == "false" ]]; then
+    tcp_status=`nmap -Pn baidu.com -p 80|grep open|wc -l`
+    if [ $tcp_status -eq 0 ]; then
         tcp_count=0
         while [[ $tcp_count -lt $check_times ]]
         do
             #如果false则多次检查确认,无true记录的话,更换ip,默认值4次
-            tcp_status=`curl --silent https://ipcheck.need.sh/api_v2.php?ip=${local_ip} | awk -F '[:}]' '{print $21}'`
-            [[ $tcp_status == "true" ]] && exit 0
+            tcp_status=`nmap -Pn baidu.com -p 80|grep open|wc -l`
+            [ $tcp_status -eq 1 ] && exit 0
             tcp_count=`expr ${tcp_count} + 1`
             sleep 2s
         done
